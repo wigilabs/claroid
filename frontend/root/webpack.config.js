5e10d39740656bc3c0fa167bfe5ffaca7caf97fd
Joredjs <odraude160@gmail.com>
2023-03-05 18:31:32 -0500

    feat(mfe): creacion login (#8)
    * docs: ajuste nombre ramas y modificacion en TODO
    
    * feat(mfe): creacion root-config
    
    * feat(mfe): creacion login

--- frontend/root/webpack.config.js
+++ frontend/root/webpack.config.js
@@ -3,7 +3,7 @@ const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (webpackConfigEnv, argv) => {
-  const orgName = "claroid";
+  const orgName = "claroid-mfe";
   const defaultConfig = singleSpaDefaults({
     orgName,
     projectName: "root-config",
